c1dbcd948c3a732374b6cb5c4f4ea494b96ab295
akarsh2413 <pandeyakarsh09@gmail.com>
2026-07-06 12:47:15 +0530

    Replace Nodemailer with Resend

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,33 +1,33 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 
-const sendEmail = async (to, subject, text = "", html = "") => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-  const mailOptions = {
-    from: `"FinanceFlow" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    text,
-    html,
-  };
-
+const sendEmail = async (
+  to,
+  subject,
+  text = "",
+  html = ""
+) => {
   try {
-    const info = await transporter.sendMail(mailOptions);
+    const { data, error } = await resend.emails.send({
+      from: "FinanceFlow <onboarding@resend.dev>",
+      to,
+      subject,
+      text,
+      html,
+    });
+
+    if (error) {
+      console.error("Resend Error:", error);
+      throw new Error(error.message);
+    }
 
     console.log("Email sent successfully");
-    console.log(info);
+    console.log(data);
 
-    return info;
+    return data;
   } catch (error) {
     console.error("Email Error:", error);
-
-    // IMPORTANT
     throw error;
   }
 };
