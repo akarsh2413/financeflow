114acf2a5b35921de1abd7bfd20c7184c3fa881d
akarsh2413 <pandeyakarsh09@gmail.com>
2026-07-06 12:21:11 +0530

    Show email sending errors

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,34 +1,34 @@
 const nodemailer = require("nodemailer");
 
-const sendEmail = async (
-  to,
-  subject,
-  text = "",
-  html = ""
-) => {
+const sendEmail = async (to, subject, text = "", html = "") => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: `"FinanceFlow" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    text,
+    html,
+  };
+
   try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-    const mailOptions = {
-      from: `"FinanceFlow" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      text,
-      html,
-    };
-    console.log(mailOptions);
-
-    await transporter.sendMail(mailOptions);
+    const info = await transporter.sendMail(mailOptions);
 
     console.log("Email sent successfully");
+    console.log(info);
+
+    return info;
   } catch (error) {
-    console.log("Email Error:", error);
+    console.error("Email Error:", error);
+
+    // IMPORTANT
+    throw error;
   }
 };
 
