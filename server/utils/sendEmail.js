9421f44b6610a08b9aa8f15c73684fbbf4b889a0
akarsh2413 <pandeyakarsh09@gmail.com>
2026-07-15 23:07:24 +0530

    Update Brevo SMTP configuration

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,12 +1,21 @@
 const nodemailer = require("nodemailer");
 
+const nodemailer = require("nodemailer");
+
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
